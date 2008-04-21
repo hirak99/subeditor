e17691db502dbceffd44d3aa5912dfceda65a707
hirak99 <hirak99@4af9b365-c64a-0410-8d1e-3168130c43b5>
2008-04-21 02:33:49 +0000

    Reformatted Adjustors.java from Netbeans Source->Reformat to convert tabs into spaces. (Also reformatted MainComponent.java in last revision.)

--- src/arnab/subedit/Adjustors.java
+++ src/arnab/subedit/Adjustors.java
@@ -21,62 +21,78 @@ import javax.swing.text.MaskFormatter;
  *
  */
 class Adjustor {
-	private int lineNum;
-	public int getLineNum() {return lineNum;}
-	public Adjustor(int line,String text,long time) {
-		lineNum=line;
-		panel=new JPanel();
-		panel.setLayout(new BoxLayout(panel,BoxLayout.X_AXIS));
-		panel.add(new JLabel((lineNum+1)+". "+text));
-		ftf=new JFormattedTextField(CreateMask());
-		ftf.setValue(Utils.toTimeString(time));
-		ftf.setMaximumSize(new Dimension(80,20));
-		panel.add(ftf);
-		ftf.addFocusListener(new FocusListener() {
-			public void focusGained(FocusEvent e) {	
-				fireFocusEvent(true);
-                        }
-			public void focusLost(FocusEvent e) {
-				fireFocusEvent(false);
-			}
-		});
-	}
-        private Vector<FocusListener> FocusListeners=new Vector<FocusListener>();
-        public void addFocusListener(FocusListener l) {
-            FocusListeners.add(l);
-        }
-        private void fireFocusEvent(boolean focussed) {
-            for (FocusListener l : FocusListeners) {
-                if (focussed) l.focusGained(null);
-                else l.focusLost(null);
+
+    private int lineNum;
+
+    public int getLineNum() {
+        return lineNum;
+    }
+
+    public Adjustor(int line, String text, long time) {
+        lineNum = line;
+        panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.X_AXIS));
+        panel.add(new JLabel((lineNum + 1) + ". " + text));
+        ftf = new JFormattedTextField(CreateMask());
+        ftf.setValue(Utils.toTimeString(time));
+        ftf.setMaximumSize(new Dimension(80, 20));
+        panel.add(ftf);
+        ftf.addFocusListener(new FocusListener() {
+
+            public void focusGained(FocusEvent e) {
+                fireFocusEvent(true);
+            }
+
+            public void focusLost(FocusEvent e) {
+                fireFocusEvent(false);
+            }
+        });
+    }
+    private Vector<FocusListener> FocusListeners = new Vector<FocusListener>();
+
+    public void addFocusListener(FocusListener l) {
+        FocusListeners.add(l);
+    }
+
+    private void fireFocusEvent(boolean focussed) {
+        for (FocusListener l : FocusListeners) {
+            if (focussed) {
+                l.focusGained(null);
+            } else {
+                l.focusLost(null);
             }
         }
-	private JPanel panel;
-	private JFormattedTextField ftf;
+    }
+    private JPanel panel;
+    private JFormattedTextField ftf;
 //	public JFormattedTextField getTextBox() {return ftf;}
-	public JPanel getPanel() {
-		return panel;
-	}
-	private MaskFormatter CreateMask() {
-		MaskFormatter format=null;
-		try {
-			format=new MaskFormatter("##:##:##,###");
-		} catch (ParseException e) {
-			System.out.println("Error: Could not create format.");
-		}
-		return format;
-	}
-	public long getNewTime() {
-		return Utils.parseTime((String)ftf.getValue());
-	}
+    public JPanel getPanel() {
+        return panel;
+    }
+
+    private MaskFormatter CreateMask() {
+        MaskFormatter format = null;
+        try {
+            format = new MaskFormatter("##:##:##,###");
+        } catch (ParseException e) {
+            System.out.println("Error: Could not create format.");
+        }
+        return format;
+    }
+
+    public long getNewTime() {
+        return Utils.parseTime((String) ftf.getValue());
+    }
 }
 
-class Adjustors extends Vector<Adjustor>{
-	public void sort() {
-		Collections.sort(this,new Comparator<Adjustor>() {
-		    public int compare(Adjustor o1, Adjustor o2) {
-		    	return o2.getLineNum()-o1.getLineNum();
-		    }
-		});
-	}
+class Adjustors extends Vector<Adjustor> {
+
+    public void sort() {
+        Collections.sort(this, new Comparator<Adjustor>() {
+
+            public int compare(Adjustor o1, Adjustor o2) {
+                return o2.getLineNum() - o1.getLineNum();
+            }
+        });
+    }
 }
